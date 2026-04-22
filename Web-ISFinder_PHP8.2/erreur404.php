2f2b04b7824a28a73a03e507f1d8a0dbf127c02f
lebeaugs <ianhafiz9999@gmail.com>
2026-04-22 15:36:57 +0200

    Migrate final display pages (blast, erreur404, under_construct) to PHP 8.4

--- Web-ISFinder_PHP8.2/erreur404.php
+++ Web-ISFinder_PHP8.2/erreur404.php
@@ -1,18 +1,27 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title>Error !</title>
+    <title>Error 404 - Page Not Found</title>
+    <meta charset="utf-8" /> 
+    <link type="text/css" rel="stylesheet" href="styles/styles.css" media="screen" />
 </head>
-<body bgcolor="#e6f1eb">
-<div align="center">
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<h3>The page
-<?php
-	$page=getenv("REQUEST_URI");
-	echo "https://www-is.biotoul.fr$page";
-?>
- does not exist on this server</h3>
-<p><a href="https://www-is.biotoul.fr/" target="_top">Return to website home page</a></p>
-</div>
+<body style="background-color: #e6f1eb;">
+    <div align="center" style="margin-top: 100px;">
+        <h3>The page 
+            <?php
+            /* 
+             * Mise à jour pour PHP 8.4 : 
+             * - Utilisation de $_SERVER au lieu de getenv pour une meilleure compatibilité.
+             * - Utilisation de htmlspecialchars() pour prévenir les failles XSS (sécurité).
+             */
+            $page = $_SERVER['REQUEST_URI'] ?? '/';
+            echo "https://www-is.biotoul.fr" . htmlspecialchars($page);
+            ?>
+            does not exist on this server
+        </h3>
+        <p>
+            <a href="https://www-is.biotoul.fr/" target="_top">Return to website home page</a>
+        </p>
+    </div>
 </body>
 </html>
